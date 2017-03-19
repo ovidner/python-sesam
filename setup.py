291c479e30cadc558ae093b7a8565bedf7002a35
Olle Vidner <olle@vidner.se>
2017-03-19 01:18:04 +0100

    Switched to Zeep, modified the public API and included WSDL. Breaking changes ahead!

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup
 setup(
     name='sesam',
     version='0.0.2',
-    py_modules=['sesam'],
+    packages=['sesam'],
+    package_data={
+        'sesam': ['wsdl/*.wsdl']
+    },
     url='https://github.com/ovidner/python-sesam',
     license='MIT',
     author='Olle Vidner',
     author_email='olle@vidner.se',
     description='',
-    dependency_links=[
-        'https://bitbucket.org/jurko/suds/get/94664ddd46a61d06862fa8fb6ba7b9e054214f57.tar.bz2#egg=suds-jurko'
-    ],
     install_requires=[
-        'suds-jurko'
+        'zeep'
     ]
 )
